f4f17be67968f8734c167fe533e45d4726ba7041
Lukas Winkler <git@lw1.at>
2020-06-02 18:36:44 +0200

    set up caching

--- acronomy/settings.py
+++ acronomy/settings.py
@@ -35,7 +35,8 @@ INSTALLED_APPS = [
     'simple_history',
     'debug_toolbar',
     'rest_framework',
-    'bootstrap4'
+    'bootstrap4',
+    "cachalot"
 ]
 
 MIDDLEWARE = [
@@ -126,6 +127,14 @@ INTERNAL_IPS = [
     '127.0.0.1',
 ]
 
+if Production:
+    CACHES = {
+        'default': {
+            'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+            'LOCATION': '127.0.0.1:11211',
+        }
+    }
+
 TAGGIT_CASE_INSENSITIVE = True
 
 SECURE_BROWSER_XSS_FILTER = True
@@ -139,3 +148,22 @@ if Production:
     SECURE_HSTS_INCLUDE_SUBDOMAINS = True
     SESSION_COOKIE_SECURE = True
     CSRF_COOKIE_SECURE = True
+
+if DEBUG:
+    DEBUG_TOOLBAR_PANELS = [
+        'debug_toolbar.panels.versions.VersionsPanel',
+        'debug_toolbar.panels.timer.TimerPanel',
+        'debug_toolbar.panels.settings.SettingsPanel',
+        'debug_toolbar.panels.headers.HeadersPanel',
+        'debug_toolbar.panels.request.RequestPanel',
+        'debug_toolbar.panels.sql.SQLPanel',
+        'debug_toolbar.panels.staticfiles.StaticFilesPanel',
+        'debug_toolbar.panels.templates.TemplatesPanel',
+        'debug_toolbar.panels.cache.CachePanel',
+        'debug_toolbar.panels.signals.SignalsPanel',
+        'debug_toolbar.panels.logging.LoggingPanel',
+        'debug_toolbar.panels.redirects.RedirectsPanel',
+        'debug_toolbar.panels.profiling.ProfilingPanel',
+    ]  # default
+
+    DEBUG_TOOLBAR_PANELS.append("cachalot.panels.CachalotPanel")
