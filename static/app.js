1e714f31891e591df8cee80a0d1d32a4e3fa983b
Lukas Winkler <git@lw1.at>
2020-06-15 19:40:55 +0200

    add footer

--- static/app.js
+++ static/app.js
@@ -1,78 +1,74 @@
 $(function () {
     $('[data-toggle="tooltip"]').tooltip()
-})
 
 
-new Autocomplete('#autocomplete', {
+    new Autocomplete('#autocomplete', {
 
-    // Search function can return a promise
-    // which resolves with an array of
-    // results. In this case we're using
-    // the Wikipedia search API.
-    search: input => {
+        search: input => {
 
-        const acroSearch = input.split(':')[0];
-        const url = "/api/acronym/?search=" + acroSearch
+            const acroSearch = input.split(':')[0];
+            const url = "/api/acronym/?search=" + acroSearch
 
-        return new Promise(resolve => {
-            if (acroSearch.length < 1) {
-                return resolve([])
-            }
+            return new Promise(resolve => {
+                if (acroSearch.length < 1) {
+                    return resolve([])
+                }
 
-            fetch(url)
-                .then(response => response.json())
-                .then(data => {
-                    resolve(data)
-                })
-        })
-    },
+                fetch(url)
+                    .then(response => response.json())
+                    .then(data => {
+                        resolve(data)
+                    })
+            })
+        },
 
-    getResultValue: result => (result.name + ": " + result.full_name),
+        getResultValue: result => (result.name + ": " + result.full_name),
 
-    // Open the selected article in
-    // a new window
-    onSubmit: result => {
-        console.log(result)
-        window.location = "/acronym/" + result.slug
-    },
-    autoSelect: true,
-})
-const input = document.querySelector('input[name="tags"]')
-input.classList.remove("form-control")
-
-document.querySelector("form").addEventListener("submit", function () {
-    const list = JSON.parse(input.value).map(function (item) {
-        return item['value'];
+        // Open the selected article in
+        // a new window
+        onSubmit: result => {
+            console.log(result)
+            window.location = "/acronym/" + result.slug
+        },
+        autoSelect: true,
     })
-    input.value = list.join(",")
-    console.log(input.value)
-    return false;
-})
-const tagify = new Tagify(input, {
-    whitelist: [],
-    maxTags: 10,
-    dropdown: {
-        maxItems: 20,
-        enabled: 0
-    }
-})
-fetch("/api/tag/")
-    .then(response => response.json())
-    .then(data =>
-        data.map(function (item) {
-            return item['name'];
+    const input = document.querySelector('input[name="tags"]')
+    input.classList.remove("form-control")
+
+    document.querySelector("form").addEventListener("submit", function () {
+        const list = JSON.parse(input.value).map(function (item) {
+            return item['value'];
         })
-    )
-    .then(data => {
-
-        tagify.settings.whitelist = data
-
+        input.value = list.join(",")
+        console.log(input.value)
+        return false;
     })
+    const tagify = new Tagify(input, {
+        whitelist: [],
+        maxTags: 10,
+        dropdown: {
+            maxItems: 20,
+            enabled: 0
+        }
+    })
+    fetch("/api/tag/")
+        .then(response => response.json())
+        .then(data =>
+            data.map(function (item) {
+                return item['name'];
+            })
+        )
+        .then(data => {
 
-const myCodeMirror = CodeMirror.fromTextArea(
-    document.getElementById("id_description_md"),
-    {
-        lineWrapping: true,
-        lineNumbers: true,
-    }
-);
+            tagify.settings.whitelist = data
+
+        })
+
+    const myCodeMirror = CodeMirror.fromTextArea(
+        document.getElementById("id_description_md"),
+        {
+            lineWrapping: true,
+            lineNumbers: true,
+        }
+    );
+})
