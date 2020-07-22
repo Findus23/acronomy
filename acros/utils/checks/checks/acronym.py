55921fde0bfb6184b974ac6b845c8078797f1303
Lukas Winkler <git@lw1.at>
2020-07-22 21:05:09 +0200

    add check for correct letter

--- acros/utils/checks/checks/acronym.py
+++ acros/utils/checks/checks/acronym.py
@@ -18,6 +18,17 @@ class LetterCheck(BaseCheck):
                     f"number of letters selected ({let_len}) not equal to letters in acronym ({acr_len})",
                     obj=acronym
                 )
+            num_letters = []
+            for num in acronym.acro_letters:
+                num_letters.append(acronym.full_name[num])
+            for letter, acro_letter in zip(num_letters, acronym.name):
+                letter = letter.lower()
+                acro_letter = acro_letter.lower()
+                if letter != acro_letter:
+                    yield CheckWarning(
+                        f"letters don't match ({letter}≠{acro_letter})",
+                        obj=acronym
+                    )
 
 
 registry.register(LetterCheck)
