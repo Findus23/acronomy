c162eba316d6c06ca34b9500f546abb8996a0ee1
Lukas Winkler <git@lw1.at>
2021-01-25 21:44:58 +0100

    add secrets for tests

---
+++ acronomy/secrets.test.py
@@ -0,0 +1,14 @@
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'just-a-testkey'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+Production = False
+
+ADS_AUTH_TOKEN = None
+DATABASES = {
+        'ENGINE': 'django.db.backends.sqlite3',
+}
+
+
+MATOMO_API_KEY = None
