e2e214449d3829f1c27b40a7908962732c4ae847
Lukas Winkler <git@lw1.at>
2023-03-21 23:30:07 +0100

    fix wikipedia fetch

--- acros/management/commands/refetch_wikipedia.py
+++ acros/management/commands/refetch_wikipedia.py
@@ -1,6 +1,8 @@
 import time
+from datetime import timedelta
 
 from django.core.management.base import BaseCommand
+from django.utils import timezone
 
 from acros.models import WikipediaLink
 
@@ -11,10 +13,12 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         links = WikipediaLink.objects.all()
         for link in links:
-            print(link)
             self.stdout.write(link.title)
+            if link.timestamp >= (timezone.now() - timedelta(days=180)):
+                print("skipped")
+                continue
             link.fetched = False
             # update_change_reason(link, "refetch_wikipedia command")
             link.clean()
             link.save()
-            time.sleep(1)
+            time.sleep(3)
